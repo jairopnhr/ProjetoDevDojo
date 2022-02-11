57c98266adf10723cf95b2f38ef405348d08d40f
Jairo Pinheiro <jairopnhr@gmail.com>
2022-02-11 17:17:34 -0300

    Projeto da aula do DevDojo

--- src/main/java/com/example/Curso/Spring/Boot/DevDojo/repository/SchoolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Curso.Spring.Boot.DevDojo.repository;
-
-import com.example.Curso.Spring.Boot.DevDojo.model.School;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface SchoolRepository extends PagingAndSortingRepository<School,Long>
-{
-
-}
--- src/main/java/com/example/Curso/Spring/Boot/DevDojo/repository/TeacherRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.Curso.Spring.Boot.DevDojo.repository;
-
-import com.example.Curso.Spring.Boot.DevDojo.model.TeacherModel;
-import org.springframework.data.repository.PagingAndSortingRepository;
-
-public interface TeacherRepository extends PagingAndSortingRepository<TeacherModel , Long>
-{
-    TeacherModel findByName(String name);
-}
